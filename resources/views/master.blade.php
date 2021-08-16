99ead019577906cce4afd52294692f2eb9866e74
vishal210198p@gmail.com <vishal210198p@gmail.com>
2021-08-16 20:56:37 +0530

    modify the resources file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,7 +20,7 @@
         </style>
     </head>
     {{View::make('header')}}
-    {{View::make('Slider')}}
+    {{View::make('product')}}
     <body class="antialiased">
      <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
